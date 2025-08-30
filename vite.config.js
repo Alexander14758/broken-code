03fa4a13982e0cbfaf3561235f5778b35113aa5d
Alexander14758 <okunboralexander002@gmail.com>
2025-08-30 14:34:04 +0100

    testing

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      '68050ffc-18b0-42cd-bd1c-7ec40efe541d-00-316fv9gm0b16p.janeway.replit.dev'
+      '68050ffc-18b0-42cd-bd1c-7ec40efe541d-00-316fv9gm0b16p.janeway.replit.dev_test'
     ]
   }
 })
